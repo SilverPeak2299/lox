32bdb6856fa3eef2c41a3641af8017009aa07099
Daniel Dickerson <83009408+SilverPeak2299@users.noreply.github.com>
2025-11-08 11:29:42 +1100

    Add dam flow parsing support

--- lox_code/lox/src/lox/AstPrinter.java
+++ lox_code/lox/src/lox/AstPrinter.java
@@ -33,4 +33,61 @@ class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
   public String visitWaterflowExpr(Expr.Waterflow expr) {
     return "(waterflow " + expr.area + ")";
   }
+
+  public String visitLiteralExpr(Expr.Literal expr) {
+    return formatLiteral(expr.value);
+  }
+
+  public String visitDamExpr(Expr.Dam expr) {
+    return "Dam(" + printDamExprValue(expr.init) + ", " +
+        printDamExprValue(expr.cap) + ", " + printDamRules(expr.rules) + ")";
+  }
+
+  private String printDamRules(Expr.DamRules rules) {
+    if (rules instanceof Expr.DamRules.Flow) {
+      Expr.DamRules.Flow flow = (Expr.DamRules.Flow) rules;
+      return "Flow(" + printDamExprValue(flow.value) + ")";
+    }
+    if (rules instanceof Expr.DamRules.IfFlow) {
+      Expr.DamRules.IfFlow ifFlow = (Expr.DamRules.IfFlow) rules;
+      return "IfFlow(cond:" + printDamExprValue(ifFlow.condition) +
+          ", value:" + printDamExprValue(ifFlow.value) +
+          ", else:" + printDamRules(ifFlow.elseBranch) + ")";
+    }
+    throw new IllegalArgumentException("Unknown dam rule type");
+  }
+
+  private String printDamExprValue(Expr expr) {
+    if (expr instanceof Expr.Binary) {
+      Expr.Binary binary = (Expr.Binary) expr;
+      return "(" + printDamExprValue(binary.left) +
+          binary.operator.lexeme +
+          printDamExprValue(binary.right) + ")";
+    }
+    if (expr instanceof Expr.Variable) {
+      return visitVariableExpr((Expr.Variable) expr);
+    }
+    if (expr instanceof Expr.Literal) {
+      return visitLiteralExpr((Expr.Literal) expr);
+    }
+    if (expr instanceof Expr.Waterflow) {
+      return visitWaterflowExpr((Expr.Waterflow) expr);
+    }
+    if (expr instanceof Expr.Dam) {
+      return visitDamExpr((Expr.Dam) expr);
+    }
+    return expr.accept(this);
+  }
+
+  private String formatLiteral(Object value) {
+    if (value == null) return "nil";
+    if (value instanceof Double) {
+      double d = (Double) value;
+      if (d == Math.rint(d)) {
+        return Long.toString((long) d);
+      }
+      return Double.toString(d);
+    }
+    return value.toString();
+  }
 }
